3418ffb2a4cffba9376395ac2467396585843efe
Christian Penn <cpenn1147@gmail.com>
2022-10-05 18:40:38 -0600

    almost done

--- arrayMethods.js
+++ arrayMethods.js
@@ -18,9 +18,11 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 // CODE HERE
-// const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+const evenNumbers = mixedNumbers.filter(function(element){
+  return element %2 === 0 
+})
 
-
+console.log(evenNumbers)
 
 ////////// PROBLEM 2 //////////
 
@@ -39,7 +41,11 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-// const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+const postTaxPrices = prices.map(function(price){
+  return price * 1.07 
+});
+
+console.log(postTaxPrices)
 
 
 
@@ -57,7 +63,10 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-// const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+const totalPopulation = populations.reduce(function(acc, cur){
+  return acc + cur
+})
+console.log(totalPopulation)
 
 
 
@@ -105,6 +114,11 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 
 // CODE HERE
 
+let total = orders.map(function(num1, num2){
+  return num1 * (1 + num2)
+})
+
+console.log(total)
 
 
 ////////// PROBLEM 6 //////////
